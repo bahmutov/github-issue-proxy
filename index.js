1031053b9bb300fdffbf5ad2a96293bde31903c8
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-11-12 15:39:53 -0500

    feat(fetching): printing timestamp when fetching the issues
    for #1

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
+var config = require('./src/config');
+
+function hasValidOptions() {
+  return config.get('user') &&
+    config.get('repo');
+}
+
+if (!hasValidOptions) {
+  console.log('use: --user <github username> --repo <user repo>');
+  process.exit(-1);
+}
+
 var fetchIssues = require('./src/fetch-issues');
 var issueUtils = require('./src/issue-utils');
 
 fetchIssues({
-  user: 'bahmutov',
-  repo: 'test-pre-git'
+  user: config.get('user'),
+  repo: config.get('repo')
 }).then(function (issues) {
   console.log(issueUtils.issuesToText(issues));
 });
--- src/fetch-issues.js
+++ src/fetch-issues.js
@@ -1,6 +1,21 @@
 var gh = require('./github-api');
 var Promise = require('bluebird');
+var moment = require('moment');
 
 var repoIssues = Promise.promisify(gh.issues.repoIssues);
 
-module.exports = repoIssues;
+function ghKey(options) {
+  return options.user + '/' + options.repo;
+}
+
+function fetchIssues(options) {
+  var key = ghKey(options);
+  var now = moment();
+
+  return repoIssues(options)
+    .tap(function (issues) {
+      console.log('fetched %s at %s', key, now);
+    });
+}
+
+module.exports = fetchIssues;
