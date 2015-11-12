350be08451238f97b9fe6edc32734e6fe3b5055b
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-11-12 16:45:49 -0500

    feat(db): Saving fetched data with cache timestamp in local pouchdb
    closes #1

--- src/config.js
+++ src/config.js
@@ -3,7 +3,9 @@ var nconf = require('nconf');
 nconf.argv()
   .env()
   .defaults({
-    debug: false
+    debug: false,
+    maxAge: 10,
+    maxAgeUnits: 'minutes'
   });
 
 module.exports = nconf;
--- src/fetch-issues.js
+++ src/fetch-issues.js
@@ -1,3 +1,6 @@
+var la = require('lazy-ass');
+var check = require('check-more-types');
+var config = require('./config');
 var gh = require('./github-api');
 var Promise = require('bluebird');
 var moment = require('moment');
@@ -9,22 +12,89 @@ function ghKey(options) {
   return options.user + '/' + options.repo;
 }
 
-function fetchIssues(options) {
+function fetchAndSave(options, fetchedRecord) {
   var key = ghKey(options);
-  var now = moment();
+  console.log('fetching issues for %s', key);
 
   return repoIssues(options)
     .tap(function (issues) {
-      console.log('fetched %s at %s', key, now);
+      console.log('fetched %s at %s', key, moment());
     })
     .tap(function save(issues) {
       var record = {
-        _id: key,
-        timestamp: now,
+        timestamp: moment().toISOString(),
         issues: issues
       };
-      return db.put(record);
+      var revision = fetchedRecord && fetchedRecord._rev;
+      console.log('saving issues for key %s, revision? %s', key, revision);
+      return Promise.resolve(
+        db.put(record, key, revision)
+      );
+    })
+    .tap(function () {
+      console.log('saved fetched issues to DB %s', key);
     });
+}
+
+function isOlderThan(maxAllowedAge, timestamp) {
+  la(timestamp, 'invalid timestamp', timestamp);
+  la(moment.isDuration(maxAllowedAge), 'not a moment duration', maxAllowedAge);
+
+  var now = moment();
+  var then = moment.isMoment(timestamp) ? timestamp : moment(timestamp);
+
+  var passed = moment.duration(now.diff(then));
+  console.log('since previous fetch passed: %s', passed.humanize());
+  console.log('our caching time limit: %s', maxAllowedAge.humanize());
+
+  then = then.add(maxAllowedAge);
+  return then.isBefore(now);
+}
+
+function isInvalidTimestamp(record) {
+  return !record || !record.timestamp;
+}
+
+function loadedFromDB(record) {
+  var ts = moment(record.timestamp);
+  console.log('loaded record from %s from DB', ts);
+
+  if (isInvalidTimestamp(record)) {
+    console.log('record is missing timestamp');
+    return Promise.reject(record);
+  }
+  var maxAllowedAge = moment.duration(
+    config.get('maxAge'),
+    config.get('maxAgeUnits')
+  );
+  if (isOlderThan(maxAllowedAge, ts)) {
+    console.log('cached version %s is older than allowed %s at time %s',
+      ts, maxAllowedAge.humanize(), moment());
+    return Promise.reject(record);
+  } else {
+    console.log('cached timestamp %s is younger than %s',
+      ts, maxAllowedAge.humanize());
+  }
+  return record;
+}
+
+function dbLoad(key) {
+  return db.get(key)
+    .catch(function (err) {
+      console.log('could not load from db %s', key);
+      console.log(err.stack);
+      return Promise.reject(err);
+    });
+}
+
+function fetchIssues(options) {
+  var key = ghKey(options);
+  console.log('loading from db for key %s', key);
+
+  return dbLoad(key)
+    .then(loadedFromDB)
+    .catch(fetchAndSave.bind(null, options))
+    .catch(console.error.bind(console));
 }
 
 module.exports = fetchIssues;
